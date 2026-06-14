4bc37587640a11dfdf50a28d63da0d0d1ee439c3
Cristian Casis <crcasis@gmail.com>
2026-06-14 23:50:18 +0100

    fix trivy

--- modules/frontend_app/storage.tf
+++ modules/frontend_app/storage.tf
@@ -1,12 +1,12 @@
-resource "google_storage_bucket" "documents" {
-  project  = var.project_id
-  location = var.region
+# resource "google_storage_bucket" "documents" {
+#   project  = var.project_id
+#   location = var.region
 
-  name = var.bucket_name
+#   name = var.bucket_name
 
-  uniform_bucket_level_access = true
+#   uniform_bucket_level_access = true
 
-  versioning {
-    enabled = true
-  }
-}
+#   versioning {
+#     enabled = true
+#   }
+# }
