8b49143631a04c34645b32f078ff5b45a10631ca
Cristian Casis <crcasis@gmail.com>
2026-06-17 14:34:32 +0100

    fix pool

--- wif.tf
+++ wif.tf
@@ -2,7 +2,7 @@ module "github_wif" {
   source = "./modules/wif"
 
   project_id         = var.hub_project_id
-  pool_id            = "github-pool"
+  pool_id            = "github-pool-pipe"
   provider_id        = "github-provider"
   github_repository  = "crcasis/sdg-infra"
   service_account_id = "github-actions"
