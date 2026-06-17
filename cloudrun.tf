716ecc74c6f4ddaac85e7e11b30f63e3fa13eb2a
Cristian Casis <crcasis@gmail.com>
2026-06-17 16:48:52 +0100

    fix project cloudrun

--- cloudrun.tf
+++ cloudrun.tf
@@ -1,7 +1,7 @@
 module "cloudrun_fe" {
   source = "./modules/cloudrun"
 
-  project_id = var.fe_project_id
+  project_id = var.hub_project_id
   region     = var.region
 
   service_name = var.service_name
