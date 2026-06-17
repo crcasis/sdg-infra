fb10b769096e155c92cd597a5f2f08a36969de1f
Cristian Casis <crcasis@gmail.com>
2026-06-17 16:54:09 +0100

    fix iam

--- modules/wif/iam.tf
+++ modules/wif/iam.tf
@@ -8,3 +8,29 @@ resource "google_project_iam_member" "github_compute_storage" {
   role    = "roles/storage.object.admin"
   member  = "serviceAccount:${google_service_account.github.email}"
 }
+
+resource "google_project_iam_member" "github_run_admin" {
+  project = var.fe_project_id
+
+  role = "roles/run.admin"
+
+  member = "serviceAccount:${module.github_wif.service_account_email}"
+}
+
+
+resource "google_project_iam_member" "github_run_sa_user" {
+  project = var.fe_project_id
+
+  role = "roles/iam.serviceAccountUser"
+
+  member = "serviceAccount:${module.github_wif.service_account_email}"
+}
+
+
+resource "google_project_iam_member" "github_storage_admin_fe" {
+  project = var.fe_project_id
+
+  role = "roles/storage.admin"
+
+  member = "serviceAccount:${module.github_wif.service_account_email}"
+}
