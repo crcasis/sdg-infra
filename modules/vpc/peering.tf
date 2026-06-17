24d0799ac12fdc63302915dac43a4cbb7e44069d
Cristian Casis <crcasis@gmail.com>
2026-06-17 14:11:32 +0100

    fix peering

--- modules/vpc/peering.tf
+++ modules/vpc/peering.tf
@@ -1,11 +1,11 @@
 resource "google_compute_network_peering" "hub_to_fe" {
   name         = "hub-to-fe"
-  network      = google_compute_network.hub.self_link
-  peer_network = google_compute_network.fe.self_link
+  network      = google_compute_network.vpc["gc-p-hub-vpc"].self_link
+  peer_network = google_compute_network.vpc["gc-p-fe-vpc"].self_link
 }
 
 resource "google_compute_network_peering" "hub_to_be" {
   name         = "hub-to-be"
-  network      = google_compute_network.hub.self_link
-  peer_network = google_compute_network.be.self_link
+  network      = google_compute_network.vpc["gc-p-hub-vpc"].self_link
+  peer_network = google_compute_network.vpc["gc-p-be-vpc"].self_link
 }
