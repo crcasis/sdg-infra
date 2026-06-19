fb49811de4fe461527db521c8523e9b4e5ad2692
Cristian Casis <crcasis@gmail.com>
2026-06-19 11:16:12 +0100

    remove resources

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
-module "vpc" {
-  source        = "./modules/vpc"
-  region        = var.region
-  vpcs          = var.vpcs
-  subnets       = var.subnets
-  proxy_subnets = var.proxy_subnets
-}
+# module "vpc" {
+#   source        = "./modules/vpc"
+#   region        = var.region
+#   vpcs          = var.vpcs
+#   subnets       = var.subnets
+#   proxy_subnets = var.proxy_subnets
+# }
--- wif.tf
+++ wif.tf
@@ -1,9 +1,9 @@
-module "github_wif" {
-  source = "./modules/wif"
+# module "github_wif" {
+#   source = "./modules/wif"
 
-  project_id         = var.hub_project_id
-  pool_id            = "github-pool-pipe"
-  provider_id        = "github-provider"
-  github_repository  = "crcasis/sdg-infra"
-  service_account_id = "github-actions"
-}
+#   project_id         = var.hub_project_id
+#   pool_id            = "github-pool-pipe"
+#   provider_id        = "github-provider"
+#   github_repository  = "crcasis/sdg-infra"
+#   service_account_id = "github-actions"
+# }
